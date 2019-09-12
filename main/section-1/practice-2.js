628c4ee4ebc57e972b1c23c99427ab9a8795dfa6
Iyan <iyan@example.com>
2019-09-13 02:53:31 +0800

    Add practice 2 .js and spec.js in main and spec folders respectively and change the directory of source tags of practice-2-run.html

---
+++ main/section-1/practice-2.js
@@ -0,0 +1,16 @@
+'use strict';
+
+function collectSameElements(collectionA, collectionB) {
+
+  var collectionC = [];
+
+  for(let i = 0; i < collectionA.length; i++)
+  {
+    if(collectionB[0].includes(collectionA[i]))
+    {
+        collectionC.push(collectionA[i]);
+    }
+  }
+
+  return collectionC;
+}
