5c468505e823b3ac2156d8b9ffd6c3092c908b32
Iyan <iyan@example.com>
2019-09-13 02:15:21 +0800

    Insert Comment For Practice 1

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -4,6 +4,7 @@ function collectSameElements(collectionA, collectionB) {
 
   var collectionC = [];
 
+  //For loop for the collection of Identical items in collection A and collection B
   for(let i = 0; i < collectionA.length; i++)
   {
     if(collectionB.includes(collectionA[i]))
